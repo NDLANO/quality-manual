fa7cd8b06d839781ab42667b85b55e0265f4a664
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro.local>
2019-09-26 10:23:25 +0200

    build menu with js, changed name of chapter 11

--- js/stilguide-ndla.js
+++ js/stilguide-ndla.js
@@ -60,3 +60,81 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   })
 }, false);
+
+/*** ADD MENU VIA JS ***/
+document.addEventListener('DOMContentLoaded', function() {
+  var mainMenu = document.getElementById('mainMenu');
+  const links = [
+    {
+      title: 'Kommunikasjons- og formidlingsprinsipper',
+      link: 'kommunikasjons-formidlingsprinsipper.html',
+    },
+    {
+      title: 'Personas',
+      link: 'personas.html',
+    },
+    {
+      title: 'Universell utforming',
+      link: 'universell-utforming.html',
+    },
+    {
+      title: 'Språk og tone',
+      link: 'sprak-og-tone.html',
+    },
+    {
+      title: 'Innholdstyper',
+      link: 'innholdstyper.html',
+    },
+    {
+      title: 'Henvisning i tekst',
+      link: 'henvisning-i-tekst.html',
+    },
+    {
+      title: 'Bruk av bilder',
+      link: 'bruk-av-bilder.html',
+    },
+    {
+      title: 'Bruk av film',
+      link: 'bruk-av-film.html',
+    },
+    {
+      title: 'Bruk av logoer',
+      link: 'bruk-av-logoer.html',
+    },
+    {
+      title: 'Bruk av lisenser',
+      link: 'bruk-av-lisenser.html',
+    },
+    {
+      title: 'Utvikle læremidler for NDLA',
+      link: 'skrive-laerestoff-for-ndlano.html',
+    },
+    {
+      title: 'Skrive for sosiale medium',
+      link: 'skrive-for-sosiale-mediumer.html',
+    },
+    {
+      title: 'Skrive nyhetssaker',
+      link: 'skrive-nyhetssaker.html',
+    },
+    {
+      title: 'Lage nyhetsbrev',
+      link: 'lage-nyhetsbrev.html',
+    },
+    {
+      title: 'Sette sammen lærestoff',
+      link: 'sette-sammen-larestoff.html',
+    },
+    {
+      title: 'Ordliste – prinsipper for ordvalg',
+      link: 'ordliste-prinsipper-for-ordvalg.html',
+    },
+  ]
+  for (var i = 0; i < links.length; i++) {
+    var hyperLink = document.createElement('a');
+    hyperLink.innerHTML = '<span class="nummerering-nav">' + (i + 1) + '.</span> ' + links[i].title;
+    hyperLink.classList.add('nav-link');
+    hyperLink.href = links[i].link;
+    mainMenu.appendChild(hyperLink);
+  }
+});
